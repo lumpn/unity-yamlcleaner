3595dc9ff0c4dd794042cb87ad2c20f5125c1d70
lumpn <lumpn@users.noreply.github.com>
2021-12-16 14:48:27 +0800

    Detecting added/removed components

--- Editor/YamlCleaner.cs
+++ Editor/YamlCleaner.cs
@@ -14,64 +14,51 @@ public sealed class YamlCleaner
     [MenuItem("GameObject/Show modifications")]
     public static void ShowModifications()
     {
-        var prefab = Selection.activeObject;
-        var modifications = PrefabUtility.GetPropertyModifications(prefab);
-        if (modifications == null)
-        {
-            Debug.Log("No modifications");
-            return;
-        }
+        var prefab = Selection.activeGameObject;
+        ShowModifications(prefab);
+        ShowAddedComponents(prefab);
+        ShowRemovedComponents(prefab);
 
-        foreach (var modification in modifications)
-        {
-            Debug.LogFormat(modification.target, "path {0}, value {1}, target {2}", modification.propertyPath, modification.value, modification.target);
-        }
-
-        var builtInModifications = modifications.Where(p => p.propertyPath.StartsWith("m_", System.StringComparison.OrdinalIgnoreCase)).ToArray();
-        PrefabUtility.SetPropertyModifications(prefab, builtInModifications);
-        EditorUtility.SetDirty(prefab);
+        //var builtInModifications = modifications.Where(p => p.propertyPath.StartsWith("m_", System.StringComparison.OrdinalIgnoreCase)).ToArray();
+        //PrefabUtility.SetPropertyModifications(prefab, builtInModifications);
+        //EditorUtility.SetDirty(prefab);
     }
 
-    [MenuItem("Assets/Debug YAML")]
-    public static void RunDebug()
+    private static void ShowModifications(GameObject prefab)
     {
-        var guids = Selection.assetGUIDs;
-        foreach (var guid in guids)
+        var mods = PrefabUtility.GetPropertyModifications(prefab);
+        if (mods != null)
         {
-            var path = AssetDatabase.GUIDToAssetPath(guid);
-            var asset = AssetDatabase.LoadAssetAtPath(path, typeof(Object));
-            var obj = new SerializedObject(asset);
-            CleanPrefab(obj);
+            foreach (var mod in mods)
+            {
+                Debug.LogFormat(mod.target, "target {0}, path {1}, value {2}", mod.target, mod.propertyPath, mod.value);
+            }
         }
     }
 
-    [MenuItem("Assets/Debug YAML (objects)")]
-    public static void RunDebug2()
+    private static void ShowRemovedComponents(GameObject prefab)
     {
-        var objects = Selection.objects;
-        var obj = new SerializedObject(objects);
-        CleanPrefab(obj);
-    }
-
-    public static void CleanPrefab(SerializedObject gameObject)
-    {
-        var componentsProperty = gameObject.FindProperty("m_Component");
-        for (int i = 0; i < componentsProperty.arraySize; i++)
+        var comps = PrefabUtility.GetRemovedComponents(prefab);
+        if (comps != null)
         {
-            var elementProperty = componentsProperty.GetArrayElementAtIndex(i);
-            var componentProperty = elementProperty.FindPropertyRelative("component");
-            var component = componentProperty.objectReferenceValue;
-            var componentObj = new SerializedObject(component);
-            Clean(componentObj);
+            foreach (var comp in comps)
+            {
+                var go = comp.containingInstanceGameObject;
+                Debug.LogFormat(go, "removed {0}", comp.assetComponent);
+            }
         }
     }
 
-    public static void Clean(SerializedObject obj)
+    private static void ShowAddedComponents(GameObject prefab)
     {
-        var property = obj.GetIterator();
-        while (property.Next(true))
+        var comps = PrefabUtility.GetAddedComponents(prefab);
+        if (comps != null)
         {
-            Debug.LogFormat("name {0}, display {1}, path {2}, type {3}, ptype {4}", property.name, property.displayName, property.propertyPath, property.type, property.propertyType);
+            foreach (var comp in comps)
+            {
+                var go = comp.instanceComponent.gameObject;
+                Debug.LogFormat(go, "added {0}", comp.instanceComponent);
+            }
         }
     }
 }
